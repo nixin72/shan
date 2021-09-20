f7bcfc542c69d79ab29ba5d76475486ce39f9872
Philip Dumaresq <phdumaresq@protonmail.com>
2021-09-20 18:14:15 -0400

    Got native-image compiling successfully

---
+++ build.clj
@@ -0,0 +1,23 @@
+(ns build
+  (:refer-clojure :exclude [compile])
+  (:require
+   [clojure.tools.build.api :as b]))
+
+(def class-dir "target/classes")
+(def version "0.0.1")
+
+(def uber-basis
+  (b/create-basis {:project "deps.edn"
+                   :aliases [:native-deps]}))
+
+(defn uber [_]
+  (println "Compiling sources sources.")
+  (b/compile-clj {:basis uber-basis
+                  :src-dirs ["src"]
+                  :class-dir class-dir
+                  :ns-compile '[shan.core]})
+  (println "Building uberjar.")
+  (b/uber {:class-dir class-dir
+           :uber-file "shan.jar"
+           :basis uber-basis
+           :main 'shan.core}))
--- doc/intro.md
+++ doc/intro.md
@@ -1,3 +1,3 @@
-# Introduction to decman
+# Introduction to shan
 
 TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
