aa264ea172764827c54a735be0771de482515e2f
Philip Dumaresq <phdumaresq@protonmail.com>
2021-10-02 17:23:14 -0400

    Added error handling for unknown or unsupported package managers

--- src/shan/managers/managers.clj
+++ src/shan/managers/managers.clj
@@ -7,27 +7,30 @@
    [shan.util :as u]))
 
 (def package-managers
-  (->> {:brew {:install "brew install"
-               :remove "brew uninstall"
-               :installed? "brew list"}
-        :yay {:install "yay -S --noconfirm"
-              :remove "yay -R --noconfirm"
-              :installed? "yay -Q"}
-        :npm {:install "npm install --global"
-              :remove "npm uninstall --global"
-              :installed? npm/installed?}
-        :pip {:install "python -m pip install"
-              :remove "python -m pip uninstall -y"
-              :installed? "python -m pip show"}
-        :gem {:install "gem install"
-              :remove "gem uninstall -x"
-              :installed? "gem list -lie"}}
-       (reduce-kv
-        (fn [a k v]
-          (cond c/windows? (= 0 (:exit (shell/sh "which" (name k))))
-                (= 0 (:exit (shell/sh "which" (name k)))) (assoc a k v)
-                :else a))
-        {})))
+  {:brew {:install "brew install"
+          :remove "brew uninstall"
+          :installed? "brew list"}
+   :yay {:install "yay -S --noconfirm"
+         :remove "yay -R --noconfirm"
+         :installed? "yay -Q"}
+   :npm {:install "npm install --global"
+         :remove "npm uninstall --global"
+         :installed? npm/installed?}
+   :pip {:install "python -m pip install"
+         :remove "python -m pip uninstall -y"
+         :installed? "python -m pip show"}
+   :gem {:install "gem install"
+         :remove "gem uninstall -x"
+         :installed? "gem list -lie"}})
+
+(def installed-managers
+  (reduce-kv (fn [a k v]
+               (if (if c/windows?
+                     (= 0 (:exit (shell/sh "cmd" "/C" "where" (name k))))
+                     (= 0 (:exit (shell/sh "which" (name k)))))
+                 (assoc a k v)
+                 a))
+             {} package-managers))
 
 (defn make-fn [command verbose?]
   (cond
@@ -38,23 +41,40 @@
                            (println "\n" out))
                          (= 0 (:exit out)))))
 
+(defn unknown-package-manager [manager]
+  (u/error (str (u/bold (name manager)) " is not a package manager known by shan.\n"
+                "Please make an issue on our GitHub repository if you want it to be included.")))
+
+(defn unavailable-package-manager [manager]
+  (u/error (str (u/bold (name manager)) " does not appear to be installed on your system.\n"
+                "If it is, please ensure that it's available in your $PATH.\n"
+                "If it is in your path and this still isn't working, please make an issue on our GitHub repository.")))
+
 (defn install-pkgs [manager pkgs verbose?]
-  (let [pm (get package-managers manager)
-        {:keys [install installed?]} pm]
-    (println "Installing" (u/bold (name manager)) "packages:")
-    (let [out (u/install-all
-               pkgs (make-fn install verbose?) (make-fn installed? verbose?))]
-      (println "")
-      (zipmap (map #(str install " " %) pkgs) out))))
+  (let [pm (get installed-managers manager)]
+    (if (nil? pm)
+      (if (contains? package-managers manager)
+        (unavailable-package-manager manager)
+        (unknown-package-manager manager))
+      (let [{:keys [install installed?]} pm
+            _ (println "Installing" (u/bold (name manager)) "packages:")
+            out (u/install-all
+                 pkgs (make-fn install verbose?) (make-fn installed? verbose?))]
+        (println "")
+        (zipmap (map #(str install " " %) pkgs) out)))))
 
 (defn remove-pkgs [manager pkgs verbose?]
-  (let [pm (get package-managers manager)
-        {:keys [remove installed?]} pm]
-    (println "Uninstalling" (u/bold (name manager)) "packages:")
-    (let [out (u/remove-all
-               pkgs (make-fn remove verbose?) (make-fn installed? verbose?))]
-      (println "")
-      (zipmap (map #(str remove " " %) pkgs) out))))
+  (let [pm (get package-managers manager)]
+    (if (nil? pm)
+      (if (contains? package-managers manager)
+        (unavailable-package-manager manager)
+        (unknown-package-manager manager))
+      (let [{:keys [remove installed?]} pm
+            _ (println "Uninstalling" (u/bold (name manager)) "packages:")
+            out (u/remove-all
+                 pkgs (make-fn remove verbose?) (make-fn installed? verbose?))]
+        (println "")
+        (zipmap (map #(str remove " " %) pkgs) out)))))
 
 (defn installed-with [pkg]
   (reduce-kv
@@ -64,8 +84,3 @@
          (conj a k) a)))
    []
    package-managers))
-
-(defn exists? [manager]
-  (if c/windows?
-    false
-    (= 0 (:exit (shell/sh "which" manager)))))
